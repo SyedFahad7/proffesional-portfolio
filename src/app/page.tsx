d43f6bea9670fc5fd63ae692b9f6e30226f891e0
Sfahad7 <sfahad2904@gmail.com>
2025-10-31 02:38:58 +0530

    apostrophe error fix with escapeing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -257,7 +257,7 @@ export default function Page() {
                   I like building things
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  During my time as a student & developer, I've attended many
+                  During my time as a student & developer, I&apos;ve attended many
                   hackathons. People from around the
                   country would come together and build incredible things in 2-3
                   days. It was eye-opening to see the endless possibilities
