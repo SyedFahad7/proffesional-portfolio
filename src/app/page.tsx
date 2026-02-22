c22341e1925ddf7bcbb1ff7c9d1cf8daeaa0636f
Sfahad7 <sfahad2904@gmail.com>
2026-02-22 14:46:15 +0530

    fix: update contact link text to reflect LinkedIn and adjust response time message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -301,13 +301,12 @@ export default function Page() {
               <p className="mx-auto max-w-[600px] font-outfit text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Want to chat? Just shoot me a dm{" "}
                 <Link
-                  href={DATA.contact.social.X.url}
+                  href={DATA.contact.social.LinkedIn.url}
                   className="text-blue-500 hover:underline font-outfit"
                 >
-                  with a direct question on twitter
+                  with a direct question on LinkedIn.
                 </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                I maybe slow to respond sometimes :)
               </p>
             </div>
           </BlurFade>
